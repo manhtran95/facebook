dc13f50a02a696a9dbcf2e2c30ace04d1c865b08
Manh <manhnus@gmail.com>
2023-04-30 00:29:43 +0700

    Models - create Friending model and run create migration

--- posts/models.py
+++ posts/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import AbstractUser, UserManager, User
 from django.core.validators import RegexValidator
 from .managers import CustomUserManager
 from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
 
@@ -38,6 +39,32 @@ class AppUser(AbstractUser):
         if self.cover_photo and 'upload/' in self.cover_photo.url:
             return self.cover_photo.url.replace('upload/', 'upload/c_fill,h_463,w_1241/')
         return ''
+
+
+class Friending(models.Model):
+    class FriendState(models.TextChoices):
+        PENDING = "PE", _("Pending")
+        FRIENDED = "FR", _("Friended")
+
+    first = models.ForeignKey(
+        AppUser, related_name='first_friending', on_delete=models.CASCADE)
+    second = models.ForeignKey(
+        AppUser, related_name='second_friending', on_delete=models.CASCADE)
+    state = models.CharField(
+        max_length=2,
+        choices=FriendState.choices,
+        default=FriendState.PENDING,
+    )
+    friend_datetime = models.DateTimeField('datetime friended', default=None)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['first', 'second'], name='friend_request')
+        ]
+        indexes = [
+            models.Index(fields=('first', 'second')),
+        ]
 
 
 class Post(models.Model):
