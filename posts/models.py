e2a3924b787aadf8cf447f8d404c9f03103a23a6
Manh <manhnus@gmail.com>
2023-05-16 13:46:37 +0700

    new model - Photo

--- posts/models.py
+++ posts/models.py
@@ -18,3 +18,18 @@ class Post(models.Model):
     @classmethod
     def make_post(cls, user):
         Post(post_text=generateSentence(), author=user).save()
+
+
+def image_file_name(instance, filename):
+    name, ext = filename.split('.')
+    file_path = 'photos/user_{user_id}/{name}.{ext}'.format(
+        user_id=instance.author.id, name=name, ext=ext)
+    return file_path
+
+
+class Photo(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    author = models.ForeignKey(AppUser, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to=image_file_name)
+    pub_datetime = models.DateTimeField(
+        'datetime published', default=timezone.now)
