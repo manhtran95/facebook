8cc46bd52111e8fb4cc45876a32d065063cdab14
Manh <manhnus@gmail.com>
2023-05-16 15:22:44 +0700

    refactor after seperate, Photos in new post - FE

--- main/static/main/scripts/profile/newPost.js
+++ main/static/main/scripts/profile/newPost.js
@@ -1,10 +1,13 @@
-export function processNewPost(endpoint) {
-    if (window.mainFriendingState != FRIENDING_STATE.Self) {
+import { createPostElement } from "./posts.js"
+
+export function processNewPost(endpoint, mainFriendingState) {
+    console.log('PROCESSING NEW POST')
+    if (mainFriendingState != window.FRIENDING_STATE.Self) {
         return
     }
     let charCount = 0
     // add style to #new-post submit button
-    let formButton = document.querySelector('#new-post button')
+    let formButton = document.querySelector('#new-post .post-button')
     let formInput = document.querySelector('#new-post textarea')
     formInput.addEventListener('input', function (e) {
         if (this.value.length > 0) {
@@ -16,19 +19,77 @@ export function processNewPost(endpoint) {
         this.style.height = (this.scrollHeight + 20) + "px";
     });
 
-    let form = document.querySelector('#new-post form')
+    // process file-image input
+    let inputField = document.querySelector(`#new-post input`)
+    // console.log(inputField)
+    // let currentFiles = []
+
+    let imagePreviewParent = document.querySelector('#new-post .image-preview-parent')
+    let eraseImageButton = document.querySelector('#new-post .erase-image-button')
+    let imageBox = document.querySelector('#new-post .image-box')
+
+    eraseImageButton.onclick = e => {
+        inputField.value = ''
+        console.log('inputField.files: ')
+        console.log(inputField.files)
+        imagePreviewParent.clearChildren()
+        imageBox.style.display = 'none';
+    };
+
+    inputField.onchange = function (e) {
+        imagePreviewParent.clearChildren()
+        let imagePreviewTemplate = document.querySelector('#new-post .image-preview-template')
+        let allFiles = []
+        console.log('haha')
+        console.log(inputField.files)
+        allFiles.push(...inputField.files)
+        allFiles.forEach((file, i) => {
+            let newImagePreview = imagePreviewTemplate.cloneNode(true)
+            newImagePreview.classList.remove('image-preview-template')
+            newImagePreview.classList.add(`image-preview${i}`)
+            imagePreviewParent.appendChild(newImagePreview)
+            let image = document.querySelector(`#new-post .image-preview${i}>img`)
+            image.src = URL.createObjectURL(file)
+            console.log(image.src)
+            newImagePreview.style.display = 'block'
+        });
+        if (imagePreviewParent.children.length > 0) {
+            imageBox.style.display = 'block';
+            let eraseImageContainer = document.querySelector('#new-post .erase-image-container')
+            if (imagePreviewParent.children.length === 1) {
+                eraseImageContainer.style.right = ''
+                eraseImageContainer.style.left = '1rem'
+            } else {
+                eraseImageContainer.style.right = '1rem'
+                eraseImageContainer.style.left = ''
+            }
+        } else {
+            imageBox.style.display = 'none';
+        }
+        // if (file2) {
+        //     imagePreview2.src = URL.createObjectURL(file2)
+        // }
+    }
+
     formButton.addEventListener('click', function (e) {
+        let form = document.querySelector('#new-post form')
+        let inputField = document.querySelector(`#new-post input`)
+        const allPosts = document.querySelector('#posts .all-posts')
         e.preventDefault()
         e.stopPropagation()
+        var formData = new FormData(form);
         const content = formInput.value;
+
+        console.log(inputField.files)
+
+        formData.append("csrfmiddlewaretoken", window.CSRF_TOKEN)
+        console.log(formData)
+
         formInput.value = '';
         formInput.style.height = "56px";
         formButton.disabled = true
 
-        axios.post(endpoint, {
-            content: content,
-            csrfmiddlewaretoken: window.CSRF_TOKEN
-        }, {
+        axios.post(endpoint, formData, {
             headers: {
                 'Content-Type': 'multipart/form-data'
             }
@@ -36,7 +97,7 @@ export function processNewPost(endpoint) {
             .then(function (response) {
                 console.log('SUCCESS!');
                 console.log(response.data);
-                allPosts.insertBefore(createPostElement(response.data.new_post), allPosts.firstChild)
+                createPostElement(response.data.new_post, 'new')
             })
             .catch(function (error) {
                 console.log('ERROR!');
--- main/static/main/scripts/profile/posts.js
+++ main/static/main/scripts/profile/posts.js
@@ -1,9 +1,48 @@
 import { getFacebookDatetimeStr } from "./../helper/helper.js"
 import { processProfileLink } from "./../main.js"
 
+export function createPostElement(p, mode = 'list') {
+    const allPosts = document.querySelector('#posts .all-posts')
+    const postTemplate = document.querySelector('#post-template');
+    let curCounter = window.curCounter
+    window.curCounter += 1
+    const newPost = postTemplate.cloneNode(true);
+    newPost.classList.add(`p${curCounter}`)
+    if (mode === 'list') {
+        allPosts.appendChild(newPost)
+    } else if (mode === 'new') {
+        allPosts.insertBefore(newPost, allPosts.firstChild)
+    } else {
+        console.log('ERROR!!')
+        return
+    }
+
+    let links = document.querySelectorAll(`.p${curCounter} .post-info a`)
+    links.forEach(link => {
+        link.href = p.author_main_url
+        processProfileLink(link)
+    })
+
+    const image = document.querySelector(`.p${curCounter} .post-info img`)
+    image.src = p.author_image
+    const name = document.querySelector(`.p${curCounter} .post-info [name='name']`)
+    name.innerText = p.author
+    const pubDatetime = document.querySelector(`.p${curCounter} .post-info [name='datetime']`)
+    pubDatetime.innerText = getFacebookDatetimeStr(new Date(p.pub_timestamp))
+
+    const postText = document.querySelector(`.p${curCounter} .post-text`)
+    postText.innerText = p.post_text
+    window.setTimeout(function () {
+        newPost.classList.add('active')
+    }, 50);
+    newPost.style.marginTop = '1.25rem'
+    newPost.style.marginBottom = '1.25rem'
+}
+
 export function processPostLoading(indexEndpoint, mainFriendingState) {
+    // reset parameters
+    window.curCounter = 0;
     let nextCounter = 0
-    let curCounter = 0
     const NUM_LOAD = 8
     let curObserve = 5
     const allPosts = document.querySelector('#posts .all-posts')
@@ -24,34 +63,6 @@ export function processPostLoading(indexEndpoint, mainFriendingState) {
         }
         window.setTimeout(observe, 50);
     })();
-
-    function createPostElement(p) {
-        curCounter += 1
-        const newPost = postTemplate.cloneNode(true);
-        newPost.classList.add(`p${curCounter}`)
-        allPosts.appendChild(newPost)
-
-        let links = document.querySelectorAll(`.p${curCounter} .post-info a`)
-        links.forEach(link => {
-            link.href = p.author_main_url
-            processProfileLink(link)
-        })
-
-        const image = document.querySelector(`.p${curCounter} .post-info img`)
-        image.src = p.author_image
-        const name = document.querySelector(`.p${curCounter} .post-info [name='name']`)
-        name.innerText = p.author
-        const pubDatetime = document.querySelector(`.p${curCounter} .post-info [name='datetime']`)
-        pubDatetime.innerText = getFacebookDatetimeStr(new Date(p.pub_timestamp))
-
-        const postText = document.querySelector(`.p${curCounter} .post-text`)
-        postText.innerText = p.post_text
-        window.setTimeout(function () {
-            newPost.classList.add('active')
-        }, 50);
-        newPost.style.marginTop = '1.25rem'
-        newPost.style.marginBottom = '1.25rem'
-    }
 
     function showPosts(posts) {
         posts.forEach(p => createPostElement(p))
@@ -107,10 +118,12 @@ export function processPostLoading(indexEndpoint, mainFriendingState) {
         let nonDisplayInfoNode = document.querySelector('#posts .posts-info .non-display')
         let displayInfoNode = document.querySelector('#posts .posts-info .display')
         if (mainFriendingState == window.FRIENDING_STATE.Self || mainFriendingState == window.FRIENDING_STATE.Friend) {
+            nonDisplayInfoNode.style.display = 'none'
             displayInfoNode.style.display = 'block'
             loadPosts();
         } else {
             nonDisplayInfoNode.style.display = 'block'
+            displayInfoNode.style.display = 'none'
             return
         }
     })();
