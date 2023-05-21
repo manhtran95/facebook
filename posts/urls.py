a3d30a0c4bbc9f7afa63a35ec5d55158a18a03ee
Manh <manhnus@gmail.com>
2023-05-21 14:32:12 +0700

    BE - Posts - Edit, Update view

--- posts/urls.py
+++ posts/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path
 from django.http import HttpResponse
 
-from .views import GeneralView, PhotoDataView
+from .views import GeneralView, PhotoDataView, EditView, UpdateView
 
 app_name = 'posts'
 
 urlpatterns = [
     path('users/<int:second_user_id>/posts',
          GeneralView.as_view(), name='create_index'),
+    path('<int:post_id>/edit', EditView.as_view(), name='edit'),
+    path('<int:post_id>/update', UpdateView.as_view(), name='update'),
     path('photos/<int:photo_id>/data',
          PhotoDataView.as_view(), name='photo_data'),
 ]
--- posts/views.py
+++ posts/views.py
@@ -12,6 +12,7 @@ from users.models import AppUser
 from friending.models import Friending
 from django import forms
 from helper.helper import compress_image, MAIN_MODE_ENUM
+import json
 
 NUM_LOAD = 8
 
@@ -92,9 +93,64 @@ class GeneralView(LoginRequiredMixin, View):
             'author_image': p.author.get_profile_picture_mini(),
             'pub_timestamp': datetime.timestamp(p.pub_datetime)*1000,
             'post_text': p.post_text,
+            'post_edit_url': reverse('posts:edit', args=(p.id,)) if state == Friending.State.self else '',
             'photos': [{
                 'image_url': pt.get_post_image(),
                 'link': reverse('posts:photo_data', args=(pt.id,)),
             } for pt in p.photo_set.all()],
         } for p in queryset]
         return JsonResponse({'page': l, 'counter': return_counter})
+
+
+class EditView(LoginRequiredMixin, View):
+    def get(self, request, post_id):
+        user = request.user
+        post = get_object_or_404(Post, pk=post_id)
+        if post.author.id != user.id:
+            return JsonResponse({'error': 'Permission denied!'})
+        data = {
+            'post_text': post.post_text,
+            'post_update_url': reverse('posts:update', args=(post.id,)),
+            'photos': [{
+                'image_url': pt.get_post_image(),
+                'id': pt.id,
+            } for pt in post.photo_set.all()],
+        }
+        return JsonResponse({'post': data})
+
+
+class UpdateView(LoginRequiredMixin, View):
+    def post(self, request, post_id):
+        user = request.user
+        post = get_object_or_404(Post, pk=post_id)
+        if post.author.id != user.id:
+            return JsonResponse({'error': 'Permission denied!'})
+
+        body = request.POST
+        post.post_text = body['content']
+        post.save()
+
+        # remove old images
+        for removed_photo_id in json.loads(body['removedPhotoIds']):
+            photo = get_object_or_404(Photo, pk=removed_photo_id)
+            photo.image.delete()
+            photo.delete()
+
+        # add new images
+        images = request.FILES.getlist('images')
+        print(images)
+
+        for oimage in images:
+            img_content = compress_image(oimage)
+            new_photo = Photo(author=user, post=post,
+                              image=img_content)
+            new_photo.save()
+
+        data = {
+            'post_text': post.post_text,
+            'photos': [{
+                'image_url': pt.get_post_image(),
+                'link': reverse('posts:photo_data', args=(pt.id,)),
+            } for pt in post.photo_set.all()],
+        }
+        return JsonResponse({'updated_post': data})
