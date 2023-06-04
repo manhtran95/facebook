20d594cc0aeade5472aacd57ca2ae9917d47bf74
Manh <manhnus@gmail.com>
2023-06-04 23:45:04 +0700

    cloudinary setting

--- facebook/settings.py
+++ facebook/settings.py
@@ -176,9 +176,9 @@ MEDIA_URL = '/media/'
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 CLOUDINARY_STORAGE = {
-    'CLOUD_NAME': env('CLOUDINARY_CLOUD_NAME'),
-    'API_KEY': env('CLOUDINARY_API_KEY'),
-    'API_SECRET': env('CLOUDINARY_API_SECRET')
+    'CLOUD_NAME': os.environ.get("CLOUDINARY_CLOUD_NAME", ''),
+    'API_KEY': os.environ.get("CLOUDINARY_API_KEY", ''),
+    'API_SECRET': os.environ.get("CLOUDINARY_API_SECRET", ''),
 }
 
 CSRF_TRUSTED_ORIGINS = ['http://*.mydomain.com',
